622e1cd8932157fc5e173480499df50f35a71904
Praveen Thamindu <praveenthamindu@gmail.com>
2022-08-19 21:15:43 +0530

    add map

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+fetch('http://ip-api.com/json/192.212.174.101')
+.then(function(response) {
+  response.json().then(jsonData => {
+    console.log(jsonData);
+  });
+})
+.catch(function(error) {
+  console.log(error)
+});

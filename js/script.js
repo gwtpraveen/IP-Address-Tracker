df16d7787d8e88bc0781a1f085fdc836c4c41645
Praveen Thamindu <praveenthamindu@gmail.com>
2022-08-19 21:17:03 +0530

    add map

--- js/script.js
+++ js/script.js
@@ -6,13 +6,3 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
-
-fetch('http://ip-api.com/json/192.212.174.101')
-.then(function(response) {
-  response.json().then(jsonData => {
-    console.log(jsonData);
-  });
-})
-.catch(function(error) {
-  console.log(error)
-});

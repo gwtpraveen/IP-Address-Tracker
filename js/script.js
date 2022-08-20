189f47c8d567be1d65ecbe00ff86e42e1b86947f
Praveen Thamindu <praveenthamindu@gmail.com>
2022-08-20 13:41:32 +0530

    Add functionality to obtain the user's IP address, display data related to that IP address, and display the user's location on a map.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,56 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+const ipAddressEl = document.getElementById("ip-address");
+const locationEl = document.getElementById("location");
+const timezoneEl = document.getElementById("timezone");
+const ispEl = document.getElementById("isp");
+const formEl = document.querySelector(".form");
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
+function generateData(ipAddress) {
+  ipAddressEl.innerText = ipAddress;
+  // fetch the data from the ip api
+  fetch(`http://ip-api.com/json/${ipAddress}`)
+    .then(res => res.json())
+    .then(data => {
+      const {city, countryCode, isp, lat, lon, zip, timezone} = data;
+      // update DOM
+      locationEl.innerText = `${city}, ${countryCode}\n${zip}`;
+      timezoneEl.innerText = timezone;
+      ispEl.innerText = isp;
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+      let map = L.map('map');
+      map.setView([lat, lon], 13);
+      // change marker icon
+      let myIcon = L.icon({
+          iconUrl: '../images/icon-location.svg'
+      });
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '© OpenStreetMap'
+      }).addTo(map);
+
+      // mark the marker 
+      let marker = L.marker([lat, lon], {icon: myIcon}).addTo(map);
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
+}
+
+
+fetch("https://api.ipify.org?format=json")
+  .then(res => res.json())
+  .then(data => {
+    generateData(data.ip);
+  })
+.catch(err => console.log(err));
+
+formEl.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputEl = e.target.firstElementChild;
+  const userIpAddress = inputEl.value;
+  const reg = /^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$/
+  if (reg.test(userIpAddress)) {
+    generateData(userIpAddress)
+  }
+})
+// 192.212.174.101
